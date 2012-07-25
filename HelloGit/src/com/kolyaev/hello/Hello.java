d7cdba1532ec6e3b5b41e1514f46f270829fd9e3
anton-kolyaev <antontyt@tut.by>
2012-07-25 18:36:01 +0300

    I hava added package com.kolyaev.hello

---
+++ HelloGit/src/com/kolyaev/hello/Hello.java
@@ -0,0 +1,13 @@
+package com.kolyaev.hello;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellooo world!!!");
+	}
+
+}
